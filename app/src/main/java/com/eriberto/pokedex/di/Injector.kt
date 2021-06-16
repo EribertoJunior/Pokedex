8cd62e1f84a22314fdfd052c7f58bb0fa90c6a99
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-16 11:58:04 -0300

    atualizando a injecao de dependencia

--- app/src/main/java/com/eriberto/pokedex/di/Injector.kt
+++ app/src/main/java/com/eriberto/pokedex/di/Injector.kt
@@ -4,14 +4,15 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 fun Application.setUpDI(){
 
     startKoin {
         androidLogger()
         androidContext(this@setUpDI)
-
-        modules(appModule)
+        androidLogger(Level.ERROR)
+        modules(listOf(appModule))
     }
 
 }
--- app/src/main/java/com/eriberto/pokedex/di/appModule.kt
+++ app/src/main/java/com/eriberto/pokedex/di/appModule.kt
@@ -1,8 +1,16 @@
 package com.eriberto.pokedex.di
 
+import com.eriberto.pokedex.MainActivityViewModel
+import com.eriberto.pokedex.network.PokeService
 import com.eriberto.pokedex.network.RetroConfig
+import com.eriberto.pokedex.pagingSource.PokemonPagingSource
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single { RetroConfig() }
+    single<PokeService> { RetroConfig().getPokeServide() }
+
+    factory { PokemonPagingSource(pokeService = get()) }
+
+    viewModel { MainActivityViewModel(pokemonPagingSource = get()) }
 }

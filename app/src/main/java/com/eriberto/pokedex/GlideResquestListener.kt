b02b3cc84e05994b49a90098d7db8eddb862da52
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-16 12:11:29 -0300

    classe listener de apoio para o Glide

---
+++ app/src/main/java/com/eriberto/pokedex/GlideResquestListener.kt
@@ -0,0 +1,31 @@
+package com.eriberto.pokedex
+
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.facebook.shimmer.ShimmerFrameLayout
+
+class GlideResquestListener(val shimerConteiner: ShimmerFrameLayout): RequestListener<Drawable> {
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: Target<Drawable>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        shimerConteiner.hideShimmer()
+        return false
+    }
+
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        shimerConteiner.hideShimmer()
+        return false
+    }
+}

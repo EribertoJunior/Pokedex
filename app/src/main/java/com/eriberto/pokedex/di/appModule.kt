06c6eabdbbec3c74aa36d8bd5558e4d30a628706
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-17 12:39:18 -0300

    injetando contexto no RetroConfig.kt

--- app/src/main/java/com/eriberto/pokedex/di/appModule.kt
+++ app/src/main/java/com/eriberto/pokedex/di/appModule.kt
@@ -1,14 +1,14 @@
 package com.eriberto.pokedex.di
 
 import com.eriberto.pokedex.MainActivityViewModel
-import com.eriberto.pokedex.network.PokeService
 import com.eriberto.pokedex.network.RetroConfig
 import com.eriberto.pokedex.pagingSource.PokemonPagingSource
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single<PokeService> { RetroConfig().getPokeServide() }
+    single { RetroConfig(androidContext()).getPokeServide() }
 
     factory { PokemonPagingSource(pokeService = get()) }
 

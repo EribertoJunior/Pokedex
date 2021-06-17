cd1108f804210f01ca06c5300433a4c8f45c6402
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-17 12:38:44 -0300

    armazenando respostas de requisicoes em cache no RetroConfig.kt

--- app/src/main/java/com/eriberto/pokedex/network/RetroConfig.kt
+++ app/src/main/java/com/eriberto/pokedex/network/RetroConfig.kt
@@ -1,24 +1,53 @@
 package com.eriberto.pokedex.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import okhttp3.Cache
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetroConfig {
+class RetroConfig(val context: Context) {
+    private val cacheSize = (5 * 1024 * 1024).toLong()
+    private val myCache = Cache(context.cacheDir, cacheSize)
 
-    companion object {
-        val baseUrl = "https://pokeapi.co/api/v2/"
-        var interceptador: HttpLoggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        var client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptador).build()
+    private val baseUrl = "https://pokeapi.co/api/v2/"
 
-        private fun getRetroInstance(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+    private var client: OkHttpClient = OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (hasNetwork(context)!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+            chain.proceed(request)
+        }
+
+        .build()
+
+    private fun getRetroInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun hasNetwork(context: Context): Boolean? {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
         }
     }
 

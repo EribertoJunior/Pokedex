8ede7fe1056940c23b2b1f6d7990266b01c4783c
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-18 11:14:36 -0300

    criado PokemonPagingSourceTest.kt

---
+++ app/src/test/java/com/eriberto/pokedex/repository/pagingSource/PokemonPagingSourceTest.kt
@@ -0,0 +1,37 @@
+package com.eriberto.pokedex.repository.pagingSource
+
+import androidx.paging.PagingSource
+import com.eriberto.pokedex.repository.model.PokemonData
+import com.eriberto.pokedex.repository.model.PokemonList
+import com.eriberto.pokedex.repository.network.PokeService
+import io.mockk.*
+import org.junit.Test
+
+class PokemonPagingSourceTest{
+
+    private val service = mockk<PokeService>()
+
+    private val mockedPokemonList = PokemonList(next = null, results = listOf(PokemonData("", "")))
+
+
+    @Test
+    fun `deve retornar lista de itens quando busca for bem sucedida`(){
+
+        val spyPokemonPagingSource = spyk(PokemonPagingSource(service))
+
+        coEvery { service.getListPokemon(offset = 0) } returns mockedPokemonList
+
+        coEvery { spyPokemonPagingSource.getNextOffSet(mockedPokemonList) } returns null
+
+        coEvery { spyPokemonPagingSource.load(any())} returns PagingSource.LoadResult.Page(data = mockedPokemonList.results, prevKey = null, nextKey = null)
+
+        coVerify { spyPokemonPagingSource.loadResultSuccess(any(), any()) }
+
+    }
+
+    @Test
+    fun `deve retornar um excepition quando a busca nao for bem sucedida`(){
+
+    }
+
+}

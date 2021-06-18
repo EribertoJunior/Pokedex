9cf9cf953e135d1f5640deab0934f565ffa187f8
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-18 19:31:43 -0300

    definindo funcao de servico suspensa

--- app/src/main/java/com/eriberto/pokedex/repository/network/PokeService.kt
+++ app/src/main/java/com/eriberto/pokedex/repository/network/PokeService.kt
@@ -1,6 +1,8 @@
 package com.eriberto.pokedex.repository.network
 
+import com.eriberto.pokedex.repository.model.PokeDetalhe
 import com.eriberto.pokedex.repository.model.PokemonList
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -14,5 +16,5 @@ interface PokeService {
     ): PokemonList
 
     @GET("pokemon/{idPokemon}")
-    suspend fun getDetalhePokemon(@Path("idPokemon") idPokemon: Int)
+    suspend fun getDetalhePokemon(@Path("idPokemon") idPokemon: Int) : Call<PokeDetalhe>
 }

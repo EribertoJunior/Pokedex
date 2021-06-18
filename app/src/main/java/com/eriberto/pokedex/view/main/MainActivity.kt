e8fca7092a113eccabe06a20e49f3becaf300330
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-18 16:18:19 -0300

    tratando status da consulta paginada

--- app/src/main/java/com/eriberto/pokedex/view/main/MainActivity.kt
+++ app/src/main/java/com/eriberto/pokedex/view/main/MainActivity.kt
@@ -5,27 +5,31 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eriberto.pokedex.viewmodel.MainActivityViewModel
 import com.eriberto.pokedex.R
 import com.eriberto.pokedex.repository.model.PokemonData
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var myAdapter: MyRecyclerViewAdapter
     private lateinit var progressBar: ProgressBar
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressBar = findViewById(R.id.progressBar)
-
         initRecyclerView()
+        initObserverLoadState()
         initViewModel()
     }
 
@@ -33,14 +37,13 @@ class MainActivity : AppCompatActivity() {
         val viewModel: MainActivityViewModel by viewModel()
         lifecycleScope.launchWhenCreated {
             viewModel.getListPokemon().collectLatest {
-                progressBar.visibility = View.GONE
                 myAdapter.submitData(it)
             }
         }
     }
 
     private fun initRecyclerView() {
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView = findViewById(R.id.recyclerView)
 
         recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -51,6 +54,20 @@ class MainActivity : AppCompatActivity() {
                 }
             })
             adapter = myAdapter
+        }
+
+    }
+
+    private fun initObserverLoadState() {
+        progressBar = findViewById(R.id.progressBar)
+        val telaDeFalha: View = findViewById(R.id.tela_de_falha)
+        lifecycleScope.launch {
+            myAdapter.loadStateFlow.collectLatest { loadState ->
+                progressBar.isVisible = loadState.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.refresh !is LoadState.Loading
+                telaDeFalha.isVisible = loadState.refresh is LoadState.Error
+
+            }
         }
     }
 }

31d3a1f6d98ac136ac525f80020cb56ad8a67100
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-18 19:26:23 -0300

    habilitando click do botao tente novamente

--- app/src/main/java/com/eriberto/pokedex/view/main/MainActivity.kt
+++ app/src/main/java/com/eriberto/pokedex/view/main/MainActivity.kt
@@ -2,18 +2,18 @@ package com.eriberto.pokedex.view.main
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.eriberto.pokedex.viewmodel.MainActivityViewModel
 import com.eriberto.pokedex.R
 import com.eriberto.pokedex.repository.model.PokemonData
+import com.eriberto.pokedex.viewmodel.MainActivityViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
     private lateinit var recyclerView: RecyclerView
 
+    private val viewModel: MainActivityViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,10 +33,17 @@ class MainActivity : AppCompatActivity() {
         initRecyclerView()
         initObserverLoadState()
         initViewModel()
+        initClickDoBotaoTentarNovamente()
+    }
+
+    private fun initClickDoBotaoTentarNovamente() {
+        val btTentarNovamente : Button= findViewById(R.id.btTentarNovamente)
+        btTentarNovamente.setOnClickListener {
+            viewModel.getListPokemon()
+        }
     }
 
     private fun initViewModel() {
-        val viewModel: MainActivityViewModel by viewModel()
         lifecycleScope.launchWhenCreated {
             viewModel.getListPokemon().collectLatest {
                 myAdapter.submitData(it)

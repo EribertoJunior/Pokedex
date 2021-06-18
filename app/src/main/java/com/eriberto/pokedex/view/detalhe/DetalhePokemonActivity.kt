bc57abd3b0687073c531d7fec57bdc8651b9c32c
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-18 19:34:29 -0300

    Activity de detalhamento criada

---
+++ app/src/main/java/com/eriberto/pokedex/view/detalhe/DetalhePokemonActivity.kt
@@ -0,0 +1,12 @@
+package com.eriberto.pokedex.view.detalhe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eriberto.pokedex.R
+
+class DetalhePokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_pokemon)
+    }
+}

28aab58381f1e68bca23eb4c7f73ef45a651017c
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-20 20:22:24 -0300

    injetando dependencias do fluxo de busca de detalhes do pokemon

--- app/src/main/java/com/eriberto/pokedex/di/appModule.kt
+++ app/src/main/java/com/eriberto/pokedex/di/appModule.kt
@@ -1,8 +1,11 @@
 package com.eriberto.pokedex.di
 
+import com.eriberto.pokedex.repository.DetalhePokemonRepo
+import com.eriberto.pokedex.repository.DetalhePokemonRepoImp
 import com.eriberto.pokedex.viewmodel.MainActivityViewModel
 import com.eriberto.pokedex.repository.network.RetroConfig
 import com.eriberto.pokedex.repository.pagingSource.PokemonPagingSource
+import com.eriberto.pokedex.viewmodel.DetalhePokemonViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -11,6 +14,8 @@ val appModule = module {
     single { RetroConfig(androidContext()).getPokeServide() }
 
     factory { PokemonPagingSource(pokeService = get()) }
+    factory<DetalhePokemonRepo> { DetalhePokemonRepoImp(pokeService = get()) }
 
     viewModel { MainActivityViewModel(pokemonPagingSource = get()) }
+    viewModel { DetalhePokemonViewModel(detalhePokemonRepo = get()) }
 }

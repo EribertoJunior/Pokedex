0a793bb05c97cfb8a39578728673931936037793
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-06-20 20:38:44 -0300

    organizacao de codigo no MyRecyclerViewAdapter.kt

--- app/src/main/java/com/eriberto/pokedex/view/main/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/eriberto/pokedex/view/main/MyRecyclerViewAdapter.kt
@@ -17,32 +17,6 @@ import com.facebook.shimmer.ShimmerFrameLayout
 class MyRecyclerViewAdapter(private val onClickListener: OnClickListener) :
     PagingDataAdapter<PokemonData, MyRecyclerViewAdapter.MeuViewHolder>(DiffUtilCallBack()) {
 
-    inner class MeuViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val tvNomePokemon = view.findViewById<TextView>(R.id.tvNomePokemon)
-        private val tvDetalhesPokemon = view.findViewById<TextView>(R.id.tvDetalhesPokemon)
-        private val ivPokemon = view.findViewById<ImageView>(R.id.ivPokemon)
-        private val ivEstrela = view.findViewById<ImageView>(R.id.ivEstrela)
-        private val shimerConteiner = view.findViewById<ShimmerFrameLayout>(R.id.shimerConteiner)
-
-        fun bind(item: PokemonData) {
-            tvNomePokemon.text = item.name
-            tvDetalhesPokemon.setOnClickListener { onClickListener.itemClick(item) }
-            shimerConteiner.startShimmer()
-            Glide.with(ivPokemon)
-                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIdPokemon(item.url)}.png")
-                .error(R.drawable.pokeboll_error)
-                .placeholder(R.drawable.pokeball_loading)
-                .listener(GlideResquestListener(shimerConteiner))
-                .circleCrop()
-                .into(ivPokemon)
-        }
-    }
-
-    private fun getIdPokemon(urlPokemon: String): String {
-        val regex = "/\\d{1,5}".toRegex()
-        return regex.find(urlPokemon)?.value?.replace("/","")?: ""
-    }
-
     override fun onBindViewHolder(holder: MeuViewHolder, position: Int) {
         getItem(position)?.let { holder.bind(it) }
     }
@@ -53,18 +27,43 @@ class MyRecyclerViewAdapter(private val onClickListener: OnClickListener) :
         return MeuViewHolder(inflate)
     }
 
+    private fun getIdPokemon(urlPokemon: String): String {
+        val regex = "/\\d{1,5}".toRegex()
+        return regex.find(urlPokemon)?.value?.replace("/","")?: ""
+    }
+
+    inner class MeuViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvNomePokemon = view.findViewById<TextView>(R.id.tvNomePokemon)
+        private val tvDetalhesPokemon = view.findViewById<TextView>(R.id.tvDetalhesPokemon)
+        private val ivPokemon = view.findViewById<ImageView>(R.id.ivPokemon)
+        private val ivEstrela = view.findViewById<ImageView>(R.id.ivEstrela)
+
+        private val shimerConteiner = view.findViewById<ShimmerFrameLayout>(R.id.shimerConteiner)
+        fun bind(item: PokemonData) {
+            tvNomePokemon.text = item.name
+            tvDetalhesPokemon.setOnClickListener { onClickListener.itemClick(getIdPokemon(item.url).toInt()) }
+            shimerConteiner.startShimmer()
+            Glide.with(ivPokemon)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIdPokemon(item.url)}.png")
+                .placeholder(R.drawable.pokeball_loading)
+                .error(R.drawable.pokeboll_error)
+                .listener(GlideResquestListener(shimerConteiner))
+                .into(ivPokemon)
+        }
+    }
+
     class DiffUtilCallBack : DiffUtil.ItemCallback<PokemonData>() {
+
         override fun areItemsTheSame(oldItem: PokemonData, newItem: PokemonData): Boolean {
             return oldItem.name == newItem.name
         }
-
         override fun areContentsTheSame(oldItem: PokemonData, newItem: PokemonData): Boolean {
             return oldItem == newItem
         }
     }
 
     interface OnClickListener {
-        fun itemClick(item: PokemonData)
+        fun itemClick(idPokemon: Int)
     }
 
 }

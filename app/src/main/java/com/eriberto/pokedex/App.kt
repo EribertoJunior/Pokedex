b8cb55dcd7e262e8ad021fe0e69df07b717ddd7d
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-07-05 20:44:11 -0300

    dispoibilizando baseUrl na classe App.kt

--- app/src/main/java/com/eriberto/pokedex/App.kt
+++ app/src/main/java/com/eriberto/pokedex/App.kt
@@ -1,11 +1,17 @@
 package com.eriberto.pokedex
 
 import android.app.Application
+import com.eriberto.pokedex.di.BASE_URL
 import com.eriberto.pokedex.di.setUpDI
 
-class App: Application() {
+open class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        BASE_URL = getBaseUrl()
         setUpDI()
+    }
+
+    protected open fun getBaseUrl(): String {
+        return BuildConfig.SERVER_URL
     }
 }

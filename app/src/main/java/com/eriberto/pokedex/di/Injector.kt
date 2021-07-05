f17071f58bf8fdcfa6d92d72076b332cb78ca7b5
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-07-05 20:46:12 -0300

    definindo a propriedade que conterá a base url inicializada na classe App.kt

--- app/src/main/java/com/eriberto/pokedex/di/Injector.kt
+++ app/src/main/java/com/eriberto/pokedex/di/Injector.kt
@@ -6,12 +6,19 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
+var BASE_URL: String = ""
+
 fun Application.setUpDI(){
 
     startKoin {
         androidLogger()
         androidContext(this@setUpDI)
         androidLogger(Level.ERROR)
+        properties(
+            mapOf(
+                PROPERTY_BASE_URL to BASE_URL
+            )
+        )
         modules(listOf(appModule))
     }
 

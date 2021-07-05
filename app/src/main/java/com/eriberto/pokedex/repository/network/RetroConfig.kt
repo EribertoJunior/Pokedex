f542a132a560a08b1886f7345e60a13f277e5c4f
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-07-05 20:39:55 -0300

    estração da configuração do OkHttpClient para a classe statica OkHttpProvider.kt e recebendo base url no construtor de RetroConfig.kt

--- app/src/main/java/com/eriberto/pokedex/repository/network/RetroConfig.kt
+++ app/src/main/java/com/eriberto/pokedex/repository/network/RetroConfig.kt
@@ -1,29 +1,16 @@
 package com.eriberto.pokedex.repository.network
 
 import android.content.Context
-import com.eriberto.pokedex.BuildConfig
-import okhttp3.Cache
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+import com.eriberto.pokedex.repository.network.OkHttpProvider.getOkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetroConfig(context: Context) {
-    private val cacheSize = (5 * 1024 * 1024).toLong()
-    private val myCache = Cache(context.cacheDir, cacheSize)
-
-    private val baseUrl = BuildConfig.SERVER_URL
-    var interceptador = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-
-    private var client: OkHttpClient = OkHttpClient.Builder()
-        .cache(myCache)
-        .addInterceptor(interceptador)
-        .build()
+class RetroConfig(private val baseUrl: String, private val context: Context) {
 
     private fun getRetroInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
-            .client(client)
+            .client(getOkHttpClient(context))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }

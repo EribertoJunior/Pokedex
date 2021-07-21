06e8549537aae9bc88186b31ea60c6d733319a9b
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-07-21 18:57:41 -0300

    criado metodos para favoritar, desfavoritar e verificar de o pokemon selecionado é favorito

--- app/src/main/java/com/eriberto/pokedex/viewmodel/detalhe/DetalhePokemonViewModel.kt
+++ app/src/main/java/com/eriberto/pokedex/viewmodel/detalhe/DetalhePokemonViewModel.kt
@@ -1,4 +1,4 @@
-package com.eriberto.pokedex.viewmodel
+package com.eriberto.pokedex.viewmodel.detalhe
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,19 +6,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eriberto.pokedex.repository.DetalhePokemonRepo
 import com.eriberto.pokedex.repository.model.PokeDetalhe
+import com.eriberto.pokedex.repository.model.PokemonData
 import com.eriberto.pokedex.repository.network.STATUS_RESULT
 import com.eriberto.pokedex.util.Result
+import com.eriberto.pokedex.viewmodel.detalhe.model.PokeDetalheData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class DetalhePokemonViewModel(private val detalhePokemonRepo: DetalhePokemonRepo) : ViewModel() {
 
     private val _detalhePokemonData = MutableLiveData<PokeDetalheData>()
-
     val detalhePokemonData: LiveData<PokeDetalheData> = _detalhePokemonData
 
-    fun getDetalhesPokemon(idPokemon: Int) {
+    private val _isFavorite = MutableLiveData<Boolean>()
+    val isFavoriteData: LiveData<Boolean> = _isFavorite
 
+    fun getDetalhesPokemon(idPokemon: Int) {
         viewModelScope.launch(Dispatchers.IO) {
             detalhePokemonRepo.getDetalhePokemon(
                 idPokemon = idPokemon,
@@ -42,13 +45,26 @@ class DetalhePokemonViewModel(private val detalhePokemonRepo: DetalhePokemonRepo
                     }
                 })
         }
-
     }
 
-    inner class PokeDetalheData(
-        var pokeDetalhe: PokeDetalhe? = null,
-        var errorMessage: String? = null,
-        var statusResult: STATUS_RESULT
-    )
+    fun favoritarPokemon(pokemonData: PokemonData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            detalhePokemonRepo.favoritarPokemon(pokemonData)
+            isFavorite(pokemonData.name)
+        }
+    }
 
+    fun desfavoritarPokemon(pokemonData: PokemonData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            detalhePokemonRepo.desfavoritarPokemon(pokemonData)
+            isFavorite(pokemonData.name)
+        }
+    }
+
+    fun isFavorite(namePokemon: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val isFavorite = detalhePokemonRepo.isFavoritePokemon(namePokemon)
+            _isFavorite.postValue(isFavorite)
+        }
+    }
 }

c6603339b27f6bd954ae14ab23115820c636579a
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-08-04 11:26:44 -0300

    usando Higher-order function

--- app/src/main/java/com/eriberto/pokedex/viewmodel/detalhe/DetalhePokemonViewModel.kt
+++ app/src/main/java/com/eriberto/pokedex/viewmodel/detalhe/DetalhePokemonViewModel.kt
@@ -4,16 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.eriberto.pokedex.repository.DetalhePokemonRepo
-import com.eriberto.pokedex.repository.model.PokeDetalhe
+import com.eriberto.pokedex.repository.PokemonRepo
 import com.eriberto.pokedex.repository.model.PokemonData
 import com.eriberto.pokedex.repository.network.STATUS_RESULT
-import com.eriberto.pokedex.util.Result
 import com.eriberto.pokedex.viewmodel.detalhe.model.PokeDetalheData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class DetalhePokemonViewModel(private val detalhePokemonRepo: DetalhePokemonRepo) : ViewModel() {
+class DetalhePokemonViewModel(private val pokemonRepo: PokemonRepo) : ViewModel() {
 
     private val _detalhePokemonData = MutableLiveData<PokeDetalheData>()
     val detalhePokemonData: LiveData<PokeDetalheData> = _detalhePokemonData
@@ -23,47 +21,45 @@ class DetalhePokemonViewModel(private val detalhePokemonRepo: DetalhePokemonRepo
 
     fun getDetalhesPokemon(idPokemon: Int) {
         viewModelScope.launch(Dispatchers.IO) {
-            detalhePokemonRepo.getDetalhePokemon(
+            pokemonRepo.getDetalhePokemon(
                 idPokemon = idPokemon,
-                result = object : Result<PokeDetalhe> {
-                    override fun success(data: PokeDetalhe) {
-                        _detalhePokemonData.postValue(
-                            PokeDetalheData(
-                                pokeDetalhe = data,
-                                statusResult = STATUS_RESULT.Success
-                            )
+                success = { detalhesDoPokemon ->
+                    _detalhePokemonData.postValue(
+                        PokeDetalheData(
+                            pokeDetalhe = detalhesDoPokemon,
+                            statusResult = STATUS_RESULT.Success
                         )
-                    }
-
-                    override fun error(errorMessage: String) {
-                        _detalhePokemonData.postValue(
-                            PokeDetalheData(
-                                errorMessage = errorMessage,
-                                statusResult = STATUS_RESULT.Error
-                            )
+                    )
+                },
+                erro = { errorMessage ->
+                    _detalhePokemonData.postValue(
+                        PokeDetalheData(
+                            errorMessage = errorMessage,
+                            statusResult = STATUS_RESULT.Error
                         )
-                    }
-                })
+                    )
+                }
+            )
         }
     }
 
     fun favoritarPokemon(pokemonData: PokemonData) {
         viewModelScope.launch(Dispatchers.IO) {
-            detalhePokemonRepo.favoritarPokemon(pokemonData)
+            pokemonRepo.favoritarPokemon(pokemonData)
             isFavorite(pokemonData.name)
         }
     }
 
     fun desfavoritarPokemon(pokemonData: PokemonData) {
         viewModelScope.launch(Dispatchers.IO) {
-            detalhePokemonRepo.desfavoritarPokemon(pokemonData)
+            pokemonRepo.desfavoritarPokemon(pokemonData)
             isFavorite(pokemonData.name)
         }
     }
 
     fun isFavorite(namePokemon: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            val isFavorite = detalhePokemonRepo.isFavoritePokemon(namePokemon)
+            val isFavorite = pokemonRepo.isFavoritePokemon(namePokemon)
             _isFavorite.postValue(isFavorite)
         }
     }

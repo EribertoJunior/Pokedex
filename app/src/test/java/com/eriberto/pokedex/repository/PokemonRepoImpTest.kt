e56a8df6d2062ae210f797a5b23d02d71d0de1cf
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-08-10 20:57:24 -0300

    correcao de testes unitarios em PokemonRepoImpTest.kt

--- app/src/test/java/com/eriberto/pokedex/repository/PokemonRepoImpTest.kt
+++ app/src/test/java/com/eriberto/pokedex/repository/PokemonRepoImpTest.kt
@@ -1,91 +1,80 @@
 package com.eriberto.pokedex.repository
 
 import com.eriberto.pokedex.repository.database.config.PokemonDatabase
-import com.eriberto.pokedex.repository.database.config.service.PokemonDAO
 import com.eriberto.pokedex.repository.model.RetornoPokemonDetalhe
 import com.eriberto.pokedex.repository.network.PokeService
-import com.eriberto.pokedex.util.Result
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
+@Suppress("UNCHECKED_CAST")
 class PokemonRepoImpTest {
-
-    private var service: PokeService = mockk()
-    private var pokemonDatabase: PokemonDatabase = mockk()
-    private var pokemonDAO: PokemonDAO = mockk()
-
-    private var resultMock = mockk<Result<RetornoPokemonDetalhe>>()
-
-    private var responseMock = mockk<retrofit2.Response<RetornoPokemonDetalhe>>()
-
-    private var callMock = mockk<Call<RetornoPokemonDetalhe>>()
+    private var pokeServiceMock: PokeService = mockk()
+    private var pokemonDatabaseMock: PokemonDatabase = mockk()
 
     @Test
-    fun `deve notificar detalhes do pokemon quando resposta for bem sucedida`() {
-        val detalhePokemonRepoImp = PokemonRepoImp(service, pokemonDatabase)
+    fun `deve notificar detalhes do pokemon quando resposta da requisicao de detalhes do pokemon for bem sucedida`() {
+        val detalhePokemonRepoImp = PokemonRepoImp(pokeServiceMock, pokemonDatabaseMock)
+        val responseRetornoPokemonDetalheMock: Response<RetornoPokemonDetalhe> = mockk()
+        val callRetornoPokemonDetalheMock: Call<RetornoPokemonDetalhe> = mockk()
+        val success: (data: RetornoPokemonDetalhe) -> Unit = {}
 
-        every { service.getDetalhePokemon(1) } returns callMock
-        every { callMock.enqueue(any<Callback<RetornoPokemonDetalhe>>()) } answers {
+        every { pokeServiceMock.getDetalhePokemon(1) } returns callRetornoPokemonDetalheMock
+        every { callRetornoPokemonDetalheMock.enqueue(any<Callback<RetornoPokemonDetalhe>>()) } answers {
             (args[0] as Callback<RetornoPokemonDetalhe>).apply {
-                onResponse(callMock, responseMock)
+                onResponse(callRetornoPokemonDetalheMock, responseRetornoPokemonDetalheMock)
             }
         }
-
-        responseMock.apply {
+        responseRetornoPokemonDetalheMock.apply {
             every { isSuccessful } returns true
             every { body() } returns mockk()
         }
-
-        every { resultMock.success(any()) } answers {}
-
-        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success = { }, erro = { }) }
-
-        coVerify { resultMock.success(any()) }
+        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success, erro = { }) }
+        coVerify { success(responseRetornoPokemonDetalheMock.body()!!) }
     }
 
     @Test
-    fun `deve notificar mensagem de erro quando resposta for mal sucedida`() {
-        val detalhePokemonRepoImp = PokemonRepoImp(service, pokemonDatabase)
+    fun `deve notificar mensagem de erro quando a resposta da requisicao de detalhes do pokemon for mal sucedida`() {
+        val detalhePokemonRepoImp = PokemonRepoImp(pokeServiceMock, pokemonDatabaseMock)
+        val responseRetornoPokemonDetalheMock: Response<RetornoPokemonDetalhe> = mockk()
+        val callRetornoPokemonDetalheMock: Call<RetornoPokemonDetalhe> = mockk()
+        val erro: (errorMessage: String) -> Unit = {}
 
-        every { service.getDetalhePokemon(1) } returns callMock
-        every { callMock.enqueue(any()) } answers {
+        every { pokeServiceMock.getDetalhePokemon(1) } returns callRetornoPokemonDetalheMock
+        every { callRetornoPokemonDetalheMock.enqueue(any()) } answers {
             (args[0] as Callback<RetornoPokemonDetalhe>).apply {
-                onResponse(callMock, responseMock)
+                onResponse(callRetornoPokemonDetalheMock, responseRetornoPokemonDetalheMock)
             }
         }
-
-        responseMock.apply {
+        responseRetornoPokemonDetalheMock.apply {
             every { isSuccessful } returns false
             every { code() } returns 400
             every { message() } returns "Bad Request"
         }
-
-        every { resultMock.error(any()) } answers {}
-
-        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success = { }, erro = { }) }
-
-        coVerify { resultMock.error(any()) }
+        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success = { }, erro) }
+        coVerify { erro(responseRetornoPokemonDetalheMock.message()) }
     }
 
     @Test
-    fun `deve notificar mensagem de erro quando a requisicao falhar`() {
-        val detalhePokemonRepoImp = PokemonRepoImp(service, pokemonDatabase)
-        every { service.getDetalhePokemon(1) } returns callMock
-        every { callMock.enqueue(any()) } answers {
+    fun `deve notificar mensagem de erro quando a requisicao de detalhes do pokemon falhar`() {
+        val detalhePokemonRepoImp = PokemonRepoImp(pokeServiceMock, pokemonDatabaseMock)
+        val callRetornoPokemonDetalheMock: Call<RetornoPokemonDetalhe> = mockk()
+        val erro: (mensagemDeErro: String) -> Unit = {}
+        val throwable = spyk(Throwable("Error"))
+
+        every { pokeServiceMock.getDetalhePokemon(1) } returns callRetornoPokemonDetalheMock
+        every { callRetornoPokemonDetalheMock.enqueue(any()) } answers {
             (args[0] as Callback<RetornoPokemonDetalhe>).apply {
-                onFailure(callMock, Throwable("Error"))
+                onFailure(callRetornoPokemonDetalheMock, throwable)
             }
         }
-        every { resultMock.error(any()) } answers {}
-
-        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success = { }, erro = { }) }
-
-        coVerify { resultMock.error(any()) }
-
+        runBlocking { detalhePokemonRepoImp.buscarDetalhesDoPokemon(1, success = { }, erro) }
+        coVerify { erro(throwable.message!!) }
     }
 }
